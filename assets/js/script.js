4ba8ac60cd1969badde07f76206d5d4510901921
ThanNguyen1 <than.nnguyen@yahoo.com>
2021-12-18 13:59:37 -0600

    adjusted fetch IMDB API function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
+var button = document.getElementById('submitButton');
+var searchInputEl = document.getElementById('search');
 
 //IMBD API
-var getNameSearch = function (name) {
-  var apiUrl = 'https://imdb-api.com/en/API/SearchName/k_1t9p2l2d/' + name;
+var searchForActor = function (actorName) {
+  var apiUrl = 'https://imdb-api.com/en/API/SearchName/k_1t9p2l2d/' + actorName;
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
-      response.json().then(function (name) {
-        displayIssues(name);
-
+      response.json().then(function (actorName) {
+        displayIssues(actorName);
       });
-    } else {
-      document.location.replace('./index.html');
-    }
+    } 
   })
         .catch(function(error){
+          //catching no actors found
             alert('Unable to find name');
         })
 };
 
-//Trakt API
+//Trakt API**Felipe
 var getNameSearch = function (id) {
-    var apiUrl = 'https://api.trakt.tv/people/' + id;
+    var traktApi = 'https://api.trakt.tv/people/' + id;
   
     fetch(apiUrl).then(function (response) {
       if (response.ok) {
