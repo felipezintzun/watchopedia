ab4b5aa98c08959f3aaae1cd5739fadd148b268c
Nicholas Nolen <nicnolen@ymail.com>
2021-12-20 08:51:17 -0600

    populated the actors section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 // GLOBAL VARIABLES
 // Declare input variable
 var inputEl = document.getElementById('search');
+// Declare the container that holds the actor information
+var actorEl = document.getElementById('actor-section');
+// Declare the actor title
+var actorTitleEl = document.getElementById('actor-title');
+// Declare the paragraph for actor information
+var actorInfoEl = document.getElementById('actor-results');
 // Declare error messages container
 var errorEl = document.getElementById('error');
 // Declare button
@@ -16,6 +22,7 @@ var getNameSearch = function (name) {
       if (response.ok) {
         response.json().then(function (name) {
           showActorOption(name);
+          displayActor(name);
         });
       } else {
         invalidInput();
@@ -26,24 +33,24 @@ var getNameSearch = function (name) {
     });
 };
 
-// Trakt API
-var getRatingsSearch = function (id) {
-  var apiUrl = 'https://api.trakt.tv/people/' + id;
+// // Trakt API
+// var getRatingsSearch = function (id) {
+//   var apiUrl = 'https://api.trakt.tv/people/' + id;
 
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (id) {
-          displayIssues(id);
-        });
-      } else {
-        invalidInput();
-      }
-    })
-    .catch(function (error) {
-      connectIssue();
-    });
-};
+//   fetch(apiUrl)
+//     .then(function (response) {
+//       if (response.ok) {
+//         response.json().then(function (id) {
+//           displayIssues(id);
+//         });
+//       } else {
+//         invalidInput();
+//       }
+//     })
+//     .catch(function (error) {
+//       connectIssue();
+//     });
+// };
 // API CALLS END
 
 // ERROR MESSAGES
@@ -62,23 +69,48 @@ var connectIssue = function () {
 // POPULATING ELEMENTS
 // Function to populate the datalist
 var showActorOption = function (name) {
-  console.log(name);
+  // define the actors array
+  var actors = name.results;
   // For loop to show all matching actor names
-  for (var i = 0; i < 5; i++) {
-    // define the actors array
-    var actors = name.results;
+  for (var i = 0; i < actors.length; i++) {
     // define 5 individual actor names
     var actor = actors[i].title;
     // if an actor matches the input
     if (actor == inputEl.value) {
       // create the option element
       var actorOption = document.createElement('option');
-      console.log(actorOption);
       // Add the actor
       actorOption.innerHTML = actor;
       inputEl.append(actorOption);
     } else {
       invalidInput();
+    }
+  }
+};
+
+var displayActor = function (name) {
+  console.log(name);
+  // define the actors array
+  var actors = name.results;
+  // For loop to show all matching actor names
+  for (var i = 0; i < actors.length; i++) {
+    // Define actors name
+    var actor = actors[i].title;
+    // Define the movies and shows the actor was in
+    var acted = actors[i].description;
+    console.log(acted);
+    // if an actor matches the input
+    if (actor == inputEl.value) {
+      // set the text content for the title
+      actorTitleEl.innerHTML =
+        'Movie and Show Information for ' + '<b>' + actor + '</b>';
+      // set the text content for the paragraph
+      actorInfoEl.textContent = acted;
+
+      // append title to the actor container
+      actorEl.append(actorTitleEl);
+      // append paragraph to the actor container
+      actorEl.append(actorInfoEl);
     }
   }
 };
