3be414b7836e8a218ffc381382c0df7f47326b52
Nicholas Nolen <nicnolen@ymail.com>
2021-12-30 15:59:26 -0600

    styled the actor section

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,22 +42,23 @@ var getMovieSearch = function (name) {
     name +
     '&page=1&include_adult=false';
 
-  fetch(movieUrl).then(function (response) {
-    //if response is good run getMovieInfo function
-    if (response.ok) {
+  fetch(movieUrl)
+    .then(function (response) {
+      //if response is good run getMovieInfo function
+      if (response.ok) {
         response.json().then(function (name) {
           showMovieInfo(name);
         });
-    } else {
+      } else {
         // otherwise run invalid input
         invalidMovie();
         return;
       }
-  })
-  //runs if there is a connection issues
-  .catch(function () {
-  connectIssue();
-  });
+    })
+    //runs if there is a connection issues
+    .catch(function () {
+      connectIssue();
+    });
 };
 
 // function to display movie info
@@ -65,11 +66,11 @@ var showMovieInfo = function (name) {
   //clear out old content
   movieSectionEl.innerHTML = '';
   movieResultsEl.innerHTML = '';
-  errorEl.textContent= '';
+  errorEl.textContent = '';
   var totalResults = name.total_results;
   if (totalResults === 0) {
-  invalidMovie();
-  return;
+    invalidMovie();
+    return;
   } else {
     // hide other sections
     movieSectionEl.classList.remove('hide');
@@ -80,17 +81,32 @@ var showMovieInfo = function (name) {
     for (let i = 0; i < movieArray.length; i++) {
       let movieInfoDiv = document.createElement('div');
       movieInfoDiv.setAttribute('id', 'movieDiv');
-      movieInfoDiv.setAttribute('style', 'width: 350px; color: white; text-align: center');
+      movieInfoDiv.setAttribute(
+        'style',
+        'width: 350px; color: white; text-align: center'
+      );
       movieInfoDiv.setAttribute('class', 'column is-one-fifth is-full-mobile');
       //generates movie posters
       let movieImage = document.createElement('img');
       movieImage.setAttribute('id', movieArray[i].title);
-      movieImage.setAttribute('alt', movieArray[i].title + ': ' + 'Image Not Available');
-      movieImage.setAttribute('src','https://image.tmdb.org/t/p/original' + movieArray[i].poster_path);
+      movieImage.setAttribute(
+        'alt',
+        movieArray[i].title + ': ' + 'Image Not Available'
+      );
+      movieImage.setAttribute(
+        'src',
+        'https://image.tmdb.org/t/p/original' + movieArray[i].poster_path
+      );
       // displays alt message if no poster available
       if (movieImage.src == 'https://image.tmdb.org/t/p/w342undefined') {
-      movieImage.removeAttribute('src', 'https://image.tmdb.org/t/p/w342undefined');
-      movieImage.setAttribute('style', 'width: 100%; color: red; font-size: 1.25em');
+        movieImage.removeAttribute(
+          'src',
+          'https://image.tmdb.org/t/p/w342undefined'
+        );
+        movieImage.setAttribute(
+          'style',
+          'width: 100%; color: red; font-size: 1.25em'
+        );
       }
       movieInfoDiv.append(movieImage);
       movieResultsEl.append(movieInfoDiv);
@@ -311,7 +327,7 @@ var showActorInfo = function (actorId) {
     actorSectionEl.append(actorInfoEl);
 
     // Make a paragraph element for the actor's biography
-    var actorBio = document.createElement('p');
+    var actorBio = document.createElement('span');
     // set the text content of the list item
     actorBio.textContent = actorBiography;
     // style the biography
